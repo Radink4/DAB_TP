7915bd2e78c1b6664f2e39ad5018f969effae2b0
Adam <adam@gmail.com>
2019-09-27 11:41:23 +0200

    Modif du nom des classes

---
+++ DabMain.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "dab.h"
+
+int main()
+{
+	int select;
+	int qtRet; qtDep;
+
+	cout << "start" << endl;
+
+	Compte co1;
+	Client cl1;
+
+	cout << "Consulter solde 1" << endl;
+	cout << "Retirer somme 2" << endl;
+	cout << "Déposer somme 3" << endl;
+	cout << "Retirer carte 4" << endl;
+	cout << "Fmenu" << endl;
+
+	switch(select)
+		case 1:
+			cout << "Solde actuel de" << cl1.nom() << cl1.prenom()<< ":"  << co1.solde() << endl;
+			break;
+
+		case 2:
+			cout << "Indiquer montant :";
+			cin << qtRet;
+
+			co1.debiter(qtRet);
+			break;
+
+		case 3:
+			cout << "Indiquer montant :";
+			cin << qtDep;
+			co1.crediter(qtDep);
+			break;
+
+		case 4:
+			//quit
+			exit
+			break;
+
+	
+	return 0;
+}
